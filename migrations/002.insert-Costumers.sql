6db30fb2c7c462b3a0228abe93764ada7115ed23
Mohau <mohaumodisana@gmail.com>
2021-10-21 12:58:19 +0200

    2nd trial on databse

--- migrations/002.insert-Costumers.sql
+++ migrations/002.insert-Costumers.sql
@@ -1,3 +1,6 @@
 
 insert into Costumers(Names, Contacts, Locations)
 values ("Mohau", 0615139745, "43 Aberdeen")
+
+insert into Costumers(Names, Contacts, Locations)
+values ({{details.name}}, {{details.contacts}}, {{details.address}})
