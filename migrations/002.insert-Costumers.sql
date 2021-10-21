d20b82fd4f2f032879404d0c651ca46d73f8e5a2
Mohau <mohaumodisana@gmail.com>
2021-10-21 13:08:55 +0200

    3rd attempt on database

--- migrations/002.insert-Costumers.sql
+++ migrations/002.insert-Costumers.sql
@@ -3,4 +3,4 @@ insert into Costumers(Names, Contacts, Locations)
 values ("Mohau", 0615139745, "43 Aberdeen")
 
 insert into Costumers(Names, Contacts, Locations)
-values ({{details.name}}, {{details.contacts}}, {{details.address}})
+values ({{login.name}}, {{login.contacts}}, {{login.address}})
